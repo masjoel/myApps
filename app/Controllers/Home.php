2a3e814e7b2d8b97f859f179df6c1f85c97818bf
masjoel <masjoel@gmail.com>
2022-02-23 13:32:17 +0700

    update DB

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,12 +8,5 @@ class Home extends BaseController
     {
         return view('welcome_message');
     }
-    function datapeserta()
-    {
-        $rec = $this->db->table('users as a')->select('a.*, b.iddosen, b.fakultas, b.progdi, b.semester, b.pembimbing, b.foto_ktm')->join('kkn_user_detail as b', 'b.iduser=a.id', 'left')->get()->getResult();
-        $data = [
-            'title' => 'Daftar Mahasiswa Bimbingan', 'uriSeg' => $this->req->uri->getSegment(1), 'rec' => $rec
-        ];
-        return view('datapeserta', $data);
-    }
+    
 }
